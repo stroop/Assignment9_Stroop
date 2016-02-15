5de5b26a1e29c3f64a4e276f666d0ecba3dd2d4d
stroop <israel941@gmail.com>
2016-02-14 23:59:44 -0600

    Final commit adds all controls, listeners, styling, and documentation

--- Assignment9_Stroop/Assignment9_Stroop/Form1.Designer.cs
+++ Assignment9_Stroop/Assignment9_Stroop/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Assignment9_Stroop
 {
-    partial class Form1
+    partial class RentalForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,26 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.mainLabel = new System.Windows.Forms.Label();
+            this.carLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.compactRBtn = new System.Windows.Forms.RadioButton();
+            this.standardRBtn = new System.Windows.Forms.RadioButton();
+            this.luxuryRBtn = new System.Windows.Forms.RadioButton();
+            this.monthCalendar = new System.Windows.Forms.MonthCalendar();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.daysLabel = new System.Windows.Forms.Label();
+            this.totalPriceLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // mainLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.mainLabel.AutoSize = true;
+            this.mainLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainLabel.Location = new System.Drawing.Point(99, 9);
+            this.mainLabel.Name = "mainLabel";
+            this.mainLabel.Size = new System.Drawing.Size(210, 25);
+            this.mainLabel.TabIndex = 0;
+            this.mainLabel.Text = "Car Rental Selection";
             // 
-            // Form1
+            // carLabel
+            // 
+            this.carLabel.AutoSize = true;
+            this.carLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carLabel.Location = new System.Drawing.Point(24, 57);
+            this.carLabel.Name = "carLabel";
+            this.carLabel.Size = new System.Drawing.Size(135, 17);
+            this.carLabel.TabIndex = 1;
+            this.carLabel.Text = "Choose A Vehicle";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLabel.Location = new System.Drawing.Point(232, 57);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(161, 17);
+            this.dateLabel.TabIndex = 2;
+            this.dateLabel.Text = "Select A Return Date";
+            // 
+            // compactRBtn
+            // 
+            this.compactRBtn.AutoSize = true;
+            this.compactRBtn.Location = new System.Drawing.Point(26, 110);
+            this.compactRBtn.Name = "compactRBtn";
+            this.compactRBtn.Size = new System.Drawing.Size(131, 17);
+            this.compactRBtn.TabIndex = 3;
+            this.compactRBtn.TabStop = true;
+            this.compactRBtn.Text = "Compact:  $19.95/day";
+            this.compactRBtn.UseVisualStyleBackColor = true;
+            // 
+            // standardRBtn
+            // 
+            this.standardRBtn.AutoSize = true;
+            this.standardRBtn.Location = new System.Drawing.Point(26, 154);
+            this.standardRBtn.Name = "standardRBtn";
+            this.standardRBtn.Size = new System.Drawing.Size(132, 17);
+            this.standardRBtn.TabIndex = 4;
+            this.standardRBtn.TabStop = true;
+            this.standardRBtn.Text = "Standard:  $24.95/day";
+            this.standardRBtn.UseVisualStyleBackColor = true;
+            // 
+            // luxuryRBtn
+            // 
+            this.luxuryRBtn.AutoSize = true;
+            this.luxuryRBtn.Location = new System.Drawing.Point(26, 199);
+            this.luxuryRBtn.Name = "luxuryRBtn";
+            this.luxuryRBtn.Size = new System.Drawing.Size(132, 17);
+            this.luxuryRBtn.TabIndex = 5;
+            this.luxuryRBtn.TabStop = true;
+            this.luxuryRBtn.Text = "Luxury:      $39.00/day";
+            this.luxuryRBtn.UseVisualStyleBackColor = true;
+            // 
+            // monthCalendar
+            // 
+            this.monthCalendar.Location = new System.Drawing.Point(201, 83);
+            this.monthCalendar.MaxSelectionCount = 1;
+            this.monthCalendar.MinDate = new System.DateTime(2000, 2, 14, 0, 0, 0, 0);
+            this.monthCalendar.Name = "monthCalendar";
+            this.monthCalendar.TabIndex = 6;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.BackColor = System.Drawing.Color.LightCyan;
+            this.submitBtn.Location = new System.Drawing.Point(27, 344);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(131, 23);
+            this.submitBtn.TabIndex = 7;
+            this.submitBtn.Text = "Selection Complete";
+            this.submitBtn.UseVisualStyleBackColor = false;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // daysLabel
+            // 
+            this.daysLabel.AutoSize = true;
+            this.daysLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.daysLabel.Location = new System.Drawing.Point(231, 270);
+            this.daysLabel.Name = "daysLabel";
+            this.daysLabel.Size = new System.Drawing.Size(117, 20);
+            this.daysLabel.TabIndex = 8;
+            this.daysLabel.Text = "Rental Days: ";
+            // 
+            // totalPriceLabel
+            // 
+            this.totalPriceLabel.AutoSize = true;
+            this.totalPriceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalPriceLabel.Location = new System.Drawing.Point(23, 270);
+            this.totalPriceLabel.Name = "totalPriceLabel";
+            this.totalPriceLabel.Size = new System.Drawing.Size(104, 20);
+            this.totalPriceLabel.TabIndex = 9;
+            this.totalPriceLabel.Text = "Total Price: ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCyan;
+            this.button1.Location = new System.Drawing.Point(235, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RentalForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(455, 421);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.totalPriceLabel);
+            this.Controls.Add(this.daysLabel);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.monthCalendar);
+            this.Controls.Add(this.luxuryRBtn);
+            this.Controls.Add(this.standardRBtn);
+            this.Controls.Add(this.compactRBtn);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.carLabel);
+            this.Controls.Add(this.mainLabel);
+            this.Name = "RentalForm";
+            this.Text = "Car Rental";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,7 +180,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label mainLabel;
+        private System.Windows.Forms.Label carLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.RadioButton compactRBtn;
+        private System.Windows.Forms.RadioButton standardRBtn;
+        private System.Windows.Forms.RadioButton luxuryRBtn;
+        private System.Windows.Forms.MonthCalendar monthCalendar;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Label daysLabel;
+        private System.Windows.Forms.Label totalPriceLabel;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Assignment9_Stroop/Assignment9_Stroop/Form1.cs
+++ Assignment9_Stroop/Assignment9_Stroop/Form1.cs
@@ -8,13 +8,66 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+Jacob Stroop
+CIS353 - Assignment 9
+2/14/16
+*/
+
 namespace Assignment9_Stroop
 {
-    public partial class Form1 : Form
+    public partial class RentalForm : Form
     {
-        public Form1()
+        public RentalForm()
         {
             InitializeComponent();
+        }
+
+        // method called when submit button is clicked
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            // declare total price variable
+            double totalPrice = 0.00;
+
+            // declare constants
+            const double Compact = 19.95;
+            const double Standard = 24.95;
+            const double Luxury = 39.00;
+
+            // calculate days by subtracting todays date from selected date
+            int days = Convert.ToInt32((monthCalendar.SelectionStart - monthCalendar.TodayDate).TotalDays);
+
+            // if user selects a present or past date, display error and return
+            if (days <= 0)
+            {
+                MessageBox.Show("Please select a future date.", "Invalid Date");
+                return;
+            }
+
+            // else/if block calculates price
+            // based on radio button currently selected
+            if (compactRBtn.Checked)
+            {
+                totalPrice = Compact * days;
+            }
+            else if (standardRBtn.Checked)
+            {
+                totalPrice = Standard * days;
+            }
+            else if(luxuryRBtn.Checked)
+            {
+                totalPrice = Luxury * days;
+            }
+
+            // set labels for number of days and total price
+            daysLabel.Text = "Rental Days: " + days;
+            totalPriceLabel.Text = "Total Price: " + totalPrice.ToString("C");
+        }
+
+        // method calls dispose to exit application
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dispose();
         }
     }
 }
